88c294051f5460347d669a190458ca6a0ed8cf44
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-04 21:46:48 +0800

    Add MVP processing

--- src/snackbar/Main.java
+++ src/snackbar/Main.java
@@ -17,30 +17,40 @@ public class Main {
         Snack pretzel = new Snack("Pretzel", 30, 2.00, foodVendingMachine.getId());
         Snack soda = new Snack("Soda", 24, 2.50, drinkVendingMachine.getId());
         Snack water = new Snack("Water", 20, 2.75, drinkVendingMachine.getId());
+        System.out.println();
 
-        System.out.println("Product ID: " + chips.getId() + " Chips quantity: " + chips.getQuantity());
-        System.out.println("Bought 10 at a cost of " + chips.getTotalCost(10));
-        System.out.println("Remaining quantity: " + chips.getQuantity());
+        jane.buySnack(soda.getTotalCost(3));
+        System.out.println("Jane cash on hand " + jane.getCash());
+        System.out.println("Quantity of Soda is " + soda.getQuantity());
+        System.out.println();
+
+        jane.buySnack(pretzel.getTotalCost(1));
+        System.out.println("Jane cash on hand " + jane.getCash());
+        System.out.println("Quantity of Pretzel is " + pretzel.getQuantity());
+        System.out.println();
+
+        bob.buySnack(soda.getTotalCost(2));
+        System.out.println("Bob cash on hand " + bob.getCash());
+        System.out.println("Quantity of Soda is " + soda.getQuantity());
+        System.out.println();
+
+        jane.addCash(10.0);
+        System.out.println("Jane cash on hand " + jane.getCash());
+        System.out.println();
         
+        jane.buySnack(chocolateBar.getTotalCost(1));
+        System.out.println("Jane cash on hand " + jane.getCash());
+        System.out.println("Quantity of Chocolate Bar is " + chocolateBar.getQuantity());
         System.out.println();
-        System.out.println("Product ID: " + chocolateBar.getId() + " Chips quantity: " + chocolateBar.getQuantity());
-        System.out.println("Bought 30 at a cost of " + chocolateBar.getTotalCost(30));
-        System.out.println("Add 4 bars to the stock");
-        chocolateBar.addQuantity(4);
-        System.out.println("Remaining quantity: " + chocolateBar.getQuantity());
 
+        pretzel.addQuantity(12);
+        System.out.println("Quantity of Pretzel is " + pretzel.getQuantity());
         System.out.println();
-        System.out.println("Update pretzel name to: " + pretzel.setName("Fresh Pretzel"));
-        System.out.println("Product ID: " + pretzel.getId() + " pretzel quantity: " + pretzel.getQuantity());
-        System.out.println("Bought 2 at a cost of " + pretzel.getTotalCost(2));
-        System.out.println("Add 100 pretzels to the stock");
-        pretzel.addQuantity(100);
-        System.out.println("Current quantity: " + pretzel.getQuantity());
-        System.out.println("Reduce price to 1.75");
-        pretzel.setCost(1.75);
-        System.out.println("Bought 2 more at a cost of " + pretzel.getTotalCost(2));
-        System.out.println("Remaining quantity: " + pretzel.getQuantity());
 
+        bob.buySnack(pretzel.getTotalCost(3));
+        System.out.println("Bob cash on hand " + bob.getCash());
+        System.out.println("Quantity of Pretzel is " + pretzel.getQuantity());
+        System.out.println();
     }
 
     public static void main(String[] args) {
