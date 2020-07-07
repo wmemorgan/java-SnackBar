0265cbb70598122b7d779add424d634f7f2ef005
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-07 23:06:15 +0800

    Create VendingMachine class

---
+++ src/snackbar/VendingMachine.java
@@ -0,0 +1,29 @@
+package snackbar;
+
+public class VendingMachine {
+    // Fields
+    private static int maxId = 0;
+
+    private int id;
+    private String name;
+
+    // Constructor
+    public VendingMachine(String name) {
+        maxId++;
+        id = maxId;
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
