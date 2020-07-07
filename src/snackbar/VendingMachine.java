80cc0e5b374d8cca9060e64eccaa97390080b4bc
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-07 23:38:15 +0800

    Add toString Override

--- src/snackbar/VendingMachine.java
+++ src/snackbar/VendingMachine.java
@@ -26,4 +26,11 @@ public class VendingMachine {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Vending Machine" + " " +
+                "Id: " + id + " " +
+                "Name: " + name; 
+    }
 }
