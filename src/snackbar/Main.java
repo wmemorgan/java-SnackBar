95ec2e5eb6872e1c072d902ac74c4fca0594587b
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-07 22:31:05 +0800

    Initialize project

---
+++ src/snackbar/Main.java
@@ -0,0 +1,11 @@
+package snackbar;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cafe Morgan's Snack Bar!");
+    }
+}
